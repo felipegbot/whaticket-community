24a5e861125cfff87b9a9a2bfa41357e5d6aa91b
Felipe Gabriel Botelho <felipeg.bot@gmail.com>
2023-10-19 14:38:13 -0300

    forks

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,11 +5,12 @@ module.exports = {
     charset: "utf8mb4",
     collate: "utf8mb4_bin"
   },
-  dialect: process.env.DB_DIALECT || "mysql",
+  dialect: process.env.DB_DIALECT || "postgres",
   timezone: "-03:00",
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
   logging: false
 };
